7436028b2841cd35f126f1d6a7c13882b463187d
mrBia <truongngochao98@gmail.com>
2019-11-21 09:07:58 +0700

    menu

--- Student_Information/Student_Information/Source.cpp
+++ Student_Information/Student_Information/Source.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
+
+struct Student {
+	int id;
+	string name;
+	float score;
+};
 
 int main()
 {
-	cout << "success";
+	while (true) {
+		int choose;
+		int exit = 0;
+		cout << "-------------MENU--------------\n";
+		cout << "1. Input\n";
+		cout << "2. Display\n";
+		cout << "3. Save to file\n";
+		cout << "4. Load from file\n";
+		cout << "0. Exit\n";
+		cout << "-----------------\n";
+		cout << "Choose: ";
+		cin >> choose;
+
+		switch (choose){
+			case 1: {
+				cout << "Input: \n";
+				while (true) {
+					int count;
+					cout << "Input count of student";
+					cin >> count;
+
+					for (int i = 0; i < count; i++) {
+						cout << "student " << i + 1 << "\n";
+						cout << "id: ";
+						
+					}
+				}
+				break;
+			}
+
+			case 0: {
+				exit = 1;
+				break;
+			}
+		}
+
+		if (exit == 1) break;
+	}
+
 	return 0;
 }
